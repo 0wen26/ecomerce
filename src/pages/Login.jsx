15616acf6efb1ab96edc29ed1fb21f18c86687e1
owen <raydroid26@gmail.com>
2022-09-08 11:41:56 +0200

    finish

---
+++ src/pages/Login.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+// container component
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/3184410/pexels-photo-3184410.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// envoltorio
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+  ${mobile({ width: "75%" })}
+`;
+
+// titulo
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+// formulario
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+// input
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+// boton
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+// link
+
+const Link = styled.a`
+  margin: 10px 0;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
